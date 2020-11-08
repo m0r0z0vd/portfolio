b16b6af6a4f2624876051b11df656df6a234cda2
m0r0z0vd <morozov.symfony@gmail.com>
2020-11-08 18:39:04 +0200

    Minor updates

--- app.sh
+++ app.sh
@@ -3,12 +3,12 @@
 set -e
 
 cd ./docker
-docker-compose down -v
+docker-compose down -v --remove-orphans
 docker-compose build
 docker-compose up -d
 docker-compose exec symfony-api composer install
 echo "Waiting for DB connection..."
-sleep 7
+sleep 10
 echo "Done."
 docker-compose exec symfony-api php bin/console doctrine:migrations:migrate --no-interaction
 cd ..
